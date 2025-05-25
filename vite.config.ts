eb7dbdf284ea409b2ae5822ae9a1b28b114e6358
muyids <289869876@qq.com>
2025-05-25 22:31:45 +0800

    api server

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,26 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+  base: '/', // Ensure base URL is set to root
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
+    host: true, // 监听所有地址
+    port: 3000,
     proxy: {
-      '/api': {
+      '^/api/.*': {
         // target: 'http://localhost:8080',
-        target: 'https://api.wristo.io',
-        changeOrigin: true, // 如果目标服务器的域名不同，这个选项需要设置为 true
-        // rewrite: (path) => path.replace(/^\/api/, ''), // 去掉路径前缀
+        target:  'https://api.wristo.io',
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       }
-    }
+    },
   },
 })
