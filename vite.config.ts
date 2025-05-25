59c62bdd84984f585b9781032d2123345bc4e951
muyids <289869876@qq.com>
2025-05-25 14:01:35 +0800

    add icon

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+        // target: 'http://localhost:8080',
+        target: 'https://api.wristo.io',
+        changeOrigin: true, // 如果目标服务器的域名不同，这个选项需要设置为 true
+        // rewrite: (path) => path.replace(/^\/api/, ''), // 去掉路径前缀
       }
     }
   },
